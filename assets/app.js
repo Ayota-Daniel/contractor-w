bd7043ecfa5834831ad9324e97842f453682adfe
nobe <ayotadanx@gmail.com>
2022-08-25 22:19:58 +0100

    page d'acceuil

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,54 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+const slidingNews1 = document.getElementById('etablish');
+const slidingNews2 = document.getElementById('sign');
+const slidingNews3 = document.getElementById('share');
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    // console.log(scrollTop, clientHeight);
+
+    const topElementToTopViewport1 = slidingNews1.getBoundingClientRect().top;
+    const topElementToTopViewport2 = slidingNews2.getBoundingClientRect().top;
+    const topElementToTopViewport3 = slidingNews3.getBoundingClientRect().top;
+
+
+    if(scrollTop > (scrollTop + topElementToTopViewport1).toFixed() - clientHeight * 0.8){
+        slidingNews1.classList.add('active');
+    }
+    if(scrollTop > (scrollTop + topElementToTopViewport2).toFixed() - clientHeight * 0.8){
+        slidingNews2.classList.add('active');
+    }
+    if(scrollTop > (scrollTop + topElementToTopViewport3).toFixed() - clientHeight * 0.8){
+        slidingNews3.classList.add('active');
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () =>{
+    new App();
+})
+
+/*const slidingNewsElt = document.querySelector('slide-in');
+
+window.addEventListener('scroll', () =>{
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    const topElementToTopViewport = slidingNewsElt.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8 ){
+        slidingNewsElt.classList.add('active');
+    }
+});*/
+
+class App{
+    constructor(){
+        this.animationAlideIn();
+    }
+
+    animationAlideIn(){
+       
+    }
+}
--- readme.md
+++ readme.md
@@ -15,18 +15,24 @@ Contractor est un site permet d'établir des contrats entre 2 ou plusieurs parti
 vous pouvez vérifier les pré-requis (sauf Docker et Docker-compose) avec la commande suivante (de la Symfony CLI) :
 
 '''bash
-symfony console check:requirements
+* symfony console check:requirements
 '''
 
 ### Lancer L'environement de développement
 
 '''bash
-docker-compose up -d
-symfony serve -d
+* docker-compose up -d
+* symfony serve -d
 '''
 ### Creer la base de donnees
 
 '''bash
-symfony console make:migration
-symfony console doctrine:migrations:migrate
+* symfony console make:migration
+* symfony console doctrine:migrations:migrate
+'''
+
+### Compiler en continue le css de tailwind
+
+'''bash
+* npx tailwindcss -i ./assets/styles/app.css -o ./public/build/app1.css --watch    
 '''
